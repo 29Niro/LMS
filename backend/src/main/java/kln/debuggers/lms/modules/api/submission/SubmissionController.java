8833bf51dd5388f354c80e8e2e485856d9c058f1
imasshaWe <imasha98.we@gmail.com>
2022-05-17 13:37:07 +0530

    add SubmissionController

---
+++ backend/src/main/java/kln/debuggers/lms/modules/api/submission/SubmissionController.java
@@ -0,0 +1,11 @@
+package kln.debuggers.lms.modules.api.submission;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("submission")
+public class SubmissionController {
+
+
+}
