fe1827947c97c4695740d048e1f64521370d7a6c
N-kapila <nirmalkapilarathne1998@gmail.com>
2022-05-22 18:47:21 +0530

    Integrate Lecture Dash Board Update

--- frontend/src/components/pages/lecturer/HomeLecturer.js
+++ frontend/src/components/pages/lecturer/HomeLecturer.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { useTheme } from '@mui/material/styles';
+import {useTheme} from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
@@ -9,8 +9,15 @@ import Chip from '@mui/material/Chip';
 import FaceIcon from '@mui/icons-material/Face';
 import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
-import { styled } from '@mui/material/styles';
-
+import {styled} from '@mui/material/styles';
+import {useEffect, useState} from "react";
+import {useNavigate} from "react-router-dom";
+import {useApi} from "../../../helpers/hookes/useApi";
+import {useAppLoading} from "../../../providers/AppLoading";
+import {useAppMessage} from "../../../providers/AppMessage";
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faUsers} from "@fortawesome/free-solid-svg-icons";
+import {useAuthUser} from "react-auth-kit";
 
 // const Item = styled(Paper)(({ theme }) => ({
 //     backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -22,41 +29,67 @@ import { styled } from '@mui/material/styles';
 
 
 function HomeLecturer() {
-    const theme = useTheme();
+    const [data, setData] = useState([]);
+    const navigate = useNavigate();
+    const api = useApi();
+    const setAppLoading = useAppLoading();
+    const appMessage = useAppMessage();
+    const auth = useAuthUser()
+
+    const init = () => {
+        setAppLoading(true)
+        api.get('course/my')
+            .then((r) => setData(r.data))
+            .catch((e) => appMessage.notifyError(e))
+            .finally(() => setAppLoading(false));
+    }
+
+    useEffect(() => {
+        init()
+    }, []);
+
     return (
         <Box>
-            <h1> Welcome lecturer</h1>
-            <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }} >
+            <h1> Welcome!</h1>
+            <h3>{`${auth().firstName} ${auth().lastName}`}</h3>
+            <Grid container rowSpacing={1} columnSpacing={{xs: 1, sm: 2, md: 3}}>
                 <Grid item xs={6}>
-                    <p>When you develop a mockup page or backend API is not ready for data fetching and you have to make 
-                        Frontend Development with static data until it comes, react-lorem-ipsum will create your gibberish texts for you.</p>
-                </Grid>
-                <Grid item xs={6}>
-                    <Box > 
-                    <img style={{width:"60%"}} src="/asset/images/Lecturer.svg"/>
+                    <p>When you develop a mockup page or backend API is not ready for data fetching and you have to make
+                        Frontend Development with static data until it comes, react-lorem-ipsum will create your
+                        gibberish texts for you.</p>
+                    <Box>
+                        <img style={{width: "60%"}} src="/asset/images/Lecturer.svg"/>
                     </Box>
                 </Grid>
+                <Grid item xs={6}>
+                    <Typography variant='h5'>My Courses</Typography>
+                    <Box sx={{display: 'flex', flexDirection: 'column'}}>
+                        {data.map((e) => {
+                            return (
+                                <Card sx={{display: 'flex',marginY:1}}>
+                                    <CardMedia
+                                        component="img"
+                                        sx={{width: 151}}
+                                        image={e.thumbnailURL}
+                                        alt=""
+                                    />
+                                    <CardContent sx={{flex: '1 0 auto'}}>
+                                        <Typography component="div" variant="h5">
+                                            {e.title}
+                                        </Typography>
+                                        <Typography variant="subtitle1" color="text.secondary" component="div">
+                                            <Chip icon={<FontAwesomeIcon icon={faUsers}/>}
+                                                  label={e.studentList.length}/>
+                                        </Typography>
+                                    </CardContent>
+                                </Card>
+                            );
+
+                        })}
+                    </Box>
+
+                </Grid>
             </Grid>
-            <h2> My courses: </h2>
-            <Card sx={{ display: 'flex' }}>
-                <CardMedia
-                    component="img"
-                    sx={{ width: 151 }}
-                    image="http://res.cloudinary.com/debuggers/image/upload/v1653029972/lms/78f4efec-c854-499e-b61f-d6747119201b.jpg"
-                    alt=""
-                />
-                <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-                    <CardContent sx={{ flex: '1 0 auto' }}>
-                        <Typography component="div" variant="h5">
-                            Course Name
-                        </Typography>
-                        <Typography variant="subtitle1" color="text.secondary" component="div">
-                            Number of Students:
-                            <Chip icon={<FaceIcon />} label="With Icon" />
-                        </Typography>
-                    </CardContent>
-                </Box>
-            </Card>
         </Box>
     );
 }
